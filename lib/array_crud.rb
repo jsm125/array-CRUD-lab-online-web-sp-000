775df1af828174b621101ed10e399043555c3075
Matt Im <matthew.seiim@gmail.com>
2020-02-22 19:20:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  []="red", "greem", "blue", "orange"
+  ["red", "greem", "blue", "orange"]
 end
 
 def add_element_to_end_of_array(array, element)
